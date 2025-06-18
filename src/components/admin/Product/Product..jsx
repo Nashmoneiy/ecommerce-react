81d13b5dee6fa78c5a5c0c1782f9e34665bdd318
Nashmoneiy <107885226+Nashmoneiy@users.noreply.github.com>
2025-06-19 00:13:23 +0100

    initial commit

--- src/components/admin/Product/Product..jsx
+++ src/components/admin/Product/Product..jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import AxiosInstance from "../../../axiosInstance";
+axiosInstance;
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axiosInstance from "../../../AxiosInstance";
 
 const Product = () => {
   const [alert, setAlert] = useState({ show: false, type: "", message: "" });
--- src/components/frontend/Productdetail.jsx
+++ src/components/frontend/Productdetail.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import AxiosInstance from "../../axiosInstance";
 import alertify from "alertifyjs";
 import { useCart } from "../../CartContext";
+import axiosInstance from "../../AxiosInstance";
+axiosInstance;
 
 const Productdetail = () => {
   const { updateCartCount } = useCart();
