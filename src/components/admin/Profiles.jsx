dd4735fb2eab6c1c71b2369a808afb5c6ba75eed
Nashmoneiy <107885226+Nashmoneiy@users.noreply.github.com>
2025-06-19 00:15:13 +0100

    initial commit

--- src/components/admin/Profiles.jsx
+++ src/components/admin/Profiles.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import AxiosInstance from "../../axiosInstance";
+axiosInstance;
 import { useNavigate } from "react-router-dom";
 import Orderdetails from "./Orderdetails";
+import axiosInstance from "../../AxiosInstance";
 
 function Profiles() {
   const [orders, setOrders] = useState([]);
